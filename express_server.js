95a1b9ab69aeb7369a30ed3251cb524502df2fb8
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-18 14:45:06 -0400

    Added a new page for adding new URLs to be shortened and included the logic to produce random 6 digit strings to serve as the shortened URL

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -13,13 +16,7 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  const templateVars = {shortURL: shortURL, longURL: urlDatabase[shortURL]};
-  res.render("urls_show", templateVars);
-});
-
-app.get("/hello", (req, res) => {
+app.get("/hello", (req, res) => { //Test Page
   const templateVars = { greeting: 'Hello World!'};
   res.render("hello_world", templateVars);
 });
@@ -29,10 +26,35 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const templateVars = {shortURL: shortURL, longURL: urlDatabase[shortURL]};
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  let shortenedURL = '';
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) {
+    shortenedURL += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return shortenedURL;
+
+}

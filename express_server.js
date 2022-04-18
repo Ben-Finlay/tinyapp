c7c85d7c31a28edbf3d725b260c77ae7928c6351
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-18 13:21:07 -0400

    added views and updated our code to make use of them.  additionally added some html to output our /urls page

--- express_server.js
+++ express_server.js
@@ -14,8 +14,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: 'Hello World!'};
+  res.render("hello_world", templateVars);
 });
+
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars)
+})
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);

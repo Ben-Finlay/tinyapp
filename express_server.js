655604701760f1cdfd8706bec4016b1622220f92
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-18 15:25:10 -0400

    added nodemon so we can make changes and test on the fly

--- express_server.js
+++ express_server.js
@@ -7,12 +7,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
+const urlDatabase = { //Example 'database'
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { //root page
   res.send("Hello!");
 });
 
@@ -21,32 +21,32 @@ app.get("/hello", (req, res) => { //Test Page
   res.render("hello_world", templateVars);
 });
 
-app.get("/urls", (req, res) => {
+app.get("/urls", (req, res) => { //list of URLS in our urlDatabase
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars)
 })
 
-app.get("/urls/new", (req, res) => {
+app.get("/urls/new", (req, res) => { //page to create a new URL link
   res.render("urls_new");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => { //individual page for each created URL
   const shortURL = req.params.shortURL;
   const templateVars = {shortURL: shortURL, longURL: urlDatabase[shortURL]};
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
+app.get("/urls.json", (req, res) => { //JSON print out of the url database - could be called with APIs
   res.json(urlDatabase);
 });
 
-app.get('/u/:shortURL', (req, res) => {
+app.get('/u/:shortURL', (req, res) => { //redirect page
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   res.redirect(`${longURL}`);
 })
 
-app.post("/urls", (req, res) => {
+app.post("/urls", (req, res) => { //add a new shortened URL
   let key = generateRandomString();
   let val = req.body.longURL;
   urlDatabase[key] = val;
@@ -57,7 +57,7 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-function generateRandomString() {
+function generateRandomString() {  //generator for new shortURL
   let shortenedURL = '';
   let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for (let i = 0; i < 6; i++) {

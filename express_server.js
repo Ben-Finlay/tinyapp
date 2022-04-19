f9bcddc41d9e76155a4e58df4be951df86ec88d2
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-19 19:33:10 -0400

    added a registration page, with code that will deliver status code 400 if nothing is entered or an existing email is registered

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const e = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -9,13 +10,30 @@ app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
+//project databases
+
 const urlDatabase = { //Example 'database'
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "someDude": {
+    id: "someDude",
+    email: "somedude@tinyapp.com",
+    password: "youllneverguessthis"
+  },
+  "someCoolDude": {
+    id: "someCoolDude",
+    email: "cooldude@tinyapp.com",
+    password: "y0u11n3v3Rgu3557H15"
+  }
+}
+
+//project get requests
+
 app.get("/", (req, res) => { //root page
-  res.redirect("/urls");
+  res.redirect("/register");
 });
 
 app.get("/hello", (req, res) => { //Test Page
@@ -24,18 +42,18 @@ app.get("/hello", (req, res) => { //Test Page
 });
 
 app.get("/urls", (req, res) => { //list of URLS in our urlDatabase
-  const templateVars = {urls: urlDatabase, username: req.cookies['username']};
+  const templateVars = {urls: urlDatabase, user: users[req.cookies['user_id']]};
   res.render("urls_index", templateVars)
 })
 
 app.get("/urls/new", (req, res) => { //page to create a new URL link
-  const templateVars = {username: req.cookies['username']}
+  const templateVars = {user: users[req.cookies['user_id']]}
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => { //individual page for each created URL
   const shortURL = req.params.shortURL;
-  const templateVars = {shortURL: shortURL, longURL: urlDatabase[shortURL], username: req.cookies['username']};
+  const templateVars = {shortURL: shortURL, longURL: urlDatabase[shortURL], user: users[req.cookies['user_id']]};
   res.render("urls_show", templateVars);
 });
 
@@ -47,7 +65,14 @@ app.get('/u/:shortURL', (req, res) => { //redirect page
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   res.redirect(`${longURL}`);
-})
+});
+
+app.get('/register', (req, res) => {
+  const templateVars = {user: users[req.cookies['user_id']]};
+  res.render("urls_register", templateVars)
+});
+
+//project post requests
 
 app.post("/urls", (req, res) => { //add a new shortened URL
   let key = generateRandomString();
@@ -72,21 +97,45 @@ app.post("/urls/:shortURL/delete", (req, res) => { //delete
 });
 
 app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
+  res.cookie('user_id', users.user_id);
   res.redirect('/urls')
 
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls')
   
 });
 
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (!email || !password) {
+    console.log(users)  
+    return res.status(400).send("Please enter a valid email and password.")  
+    
+  }
+  if (!emailDupe(email)) {
+    return res.status(400).send("Email already registered.")  
+  }
+  
+  const userID = generateRandomString();
+  users[userID] = { id: userID, 
+    email: email, 
+    password: password };
+    console.log(users)
+  res.cookie('user_id', userID);
+  res.redirect('/urls')
+})
+
+//server loop
 
 app.listen(PORT, () => {
   console.log(`Tiny App listening on port ${PORT}!`);
 });
+
+//project functions
 
 function generateRandomString() {  //generator for new shortURL
   let shortenedURL = '';
@@ -97,3 +146,15 @@ function generateRandomString() {  //generator for new shortURL
   return shortenedURL;
 
 }
+
+const emailDupe = function(eMail) {
+  for (key in users) {
+    if(users[key].email === eMail) {
+      return false;
+    }
+  }
+  return true;
+}
+
+      // res.status(400);
+      // res.send("Email already registered.")
